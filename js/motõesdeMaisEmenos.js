11603fd23406fe52bea6eb03d1ac29927c1cfe92
Alcagana <cleidetejano1504@gmail.com>
2024-08-27 20:15:25 -0300

    testando

---
+++ js/motõesdeMaisEmenos.js
@@ -0,0 +1,13 @@
+
+var contador = document.querySelector('#qtd');
+
+document.querySelector('#mais').addEventListener('click', function(){
+  var numero = parseInt(contador.textContent) + 1;
+  contador.textContent = numero;
+});
+
+document.querySelector('#menos').addEventListener
+('click', function(){
+  var numero = parseInt(contador.textContent) - 1;
+  contador.textContent = numero;
+});
