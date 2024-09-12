5239d346588ad76f46de5015e5b752484f78574c
Alcagana <153103818+Alcagana@users.noreply.github.com>
2024-09-12 17:14:10 -0300

    teste

--- tccDo-uraGelada/js/motõesdeMaisEmenos.js
+++ tccDo-uraGelada/js/motõesdeMaisEmenos.js
@@ -1,19 +1,13 @@
-var contador = document.querySelector('#qtd'); // #qtd virou uma variavel que agora pode ser manipulada
+var contador = document.querySelector('#qtd');
 
-document.querySelector('#mais').addEventListener('click', 
-  
-  function(){//função anônima
-  var numero = parseInt(contador.textContent) + 1; //saiu de string para um número inteiro e adciona quando clicado
-  contador.textContent = numero; 
+document.querySelector('#mais').addEventListener('click', function(){
+  var numero = parseInt(contador.textContent) + 1;
+  contador.textContent = numero;
 });
 
-document.querySelector('#menos').addEventListener('click',
-  
-  function(){ 
+document.querySelector('#menos').addEventListener
+('click', function(){
   var numero = parseInt(contador.textContent) - 1;
-  contador.textContent = numero; 
-
-
+  contador.textContent = numero;
 });
-
 
