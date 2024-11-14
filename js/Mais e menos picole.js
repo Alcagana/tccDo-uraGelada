15f98c6f5ac3b6c383857c8f8489490c4fb83c4c
Alcagana <cleidetejano1504@gmail.com>
2024-11-14 17:56:05 -0300

    Aline- Coloquei os botões da tela picolés

--- js/Mais e menos picole.js
+++ js/Mais e menos picole.js
@@ -1,9 +1,13 @@
 precos = {
 
     picoleChocolate: 3,
-    picoleMorango: 2.50,
-    picoleMaracuja: 2.50,
-  
+    picoleLimão: 2.51,
+    picoleMorango: 2.52,
+    picoleCoco: 2.53,
+    picoleManga: 2.54,
+    picoleFuVe: 2.55,
+    picoleMelancia: 2.56,
+    picoleAbacaxi: 2.57,  
   }
   
   //BOTÃO PICOLÉ-CHOCOLATE
@@ -44,7 +48,7 @@ precos = {
   
   }
 
-  //BOTÃO PICOLÉ-MORANGO
+  //BOTÃO PICOLÉ-LIMÃO
   const contador2 = document.querySelector('#qtd2');
   const btnMais2 = document.querySelector('#btn2mais')
   const btnMenos2 = document.querySelector('#btn2menos')
@@ -53,17 +57,17 @@ precos = {
   btnMais2.addEventListener('click',addProduto2)
   btnMenos2.addEventListener('click', removeProduto2)
   
-  function picoleMorango(){  
+  function picoleLimao(){  
     
     let quantidade2 = contador2.textContent
-    const total2 = Number(quantidade2) * Number(precos.picoleMorango) 
+    const total2 = Number(quantidade2) * Number(precos.picoleLimão) 
     textoPagar2.textContent = total2
   }
   
   function addProduto2 (){
     const numero2 = parseInt(contador2.textContent) + 1 ;
-    contador2.textContent = numero2 ;  
-    picoleMorango()
+    contador2.textContent = numero2;  
+    picoleLimao()
     
   }
   
@@ -72,7 +76,7 @@ precos = {
     if( contador2.textContent >= 1){
       let numero2 = parseInt(contador2.textContent) - 1 ;
       contador2.textContent = numero2
-      picoleMorango()
+      picoleLimao()
      
     } else{
       contador2.textContent = 0
@@ -82,7 +86,7 @@ precos = {
   
   }
 
-  //BOTÃO PICOLÉ-MARACUJÁ
+  //BOTÃO PICOLÉ-MORANGO
   const contador3 = document.querySelector('#qtd3');
   const btnMais3 = document.querySelector('#btn3mais')
   const btnMenos3 = document.querySelector('#btn3menos')
@@ -91,17 +95,17 @@ precos = {
   btnMais3.addEventListener('click',addProduto3)
   btnMenos3.addEventListener('click', removeProduto3)
   
-  function picoleMaracuja(){  
+  function picoleMorango(){  
     
     let quantidade3 = contador3.textContent
-    const total3 = Number(quantidade3) * Number(precos.picoleMaracuja) 
+    const total3 = Number(quantidade3) * Number(precos.picoleMorango) 
     textoPagar3.textContent = total3
   }
   
   function addProduto3 (){
     const numero3 = parseInt(contador3.textContent) + 1 ;
-    contador3.textContent = numero3;  
-    picoleMaracuja()
+    contador3.textContent = numero3 ;  
+    picoleMorango()
     
   }
   
@@ -110,7 +114,7 @@ precos = {
     if( contador3.textContent >= 1){
       let numero3 = parseInt(contador3.textContent) - 1 ;
       contador3.textContent = numero3
-      picoleMaracuja()
+      picoleMorango()
      
     } else{
       contador3.textContent = 0
@@ -119,5 +123,189 @@ precos = {
     }
   
   }
+  //BOTÃO PICOLÉ-COCO
+  const contador4 = document.querySelector('#qtd4');
+  const btnMais4 = document.querySelector('#btn4mais')
+  const btnMenos4 = document.querySelector('#btn4menos')
+  const textoPagar4 = document.querySelector('#total4') 
+  
+  btnMais4.addEventListener('click',addProduto4)
+  btnMenos4.addEventListener('click', removeProduto4)
+  
+  function picoleCoco(){  
+    
+    let quantidade4 = contador4.textContent
+    const total4 = Number(quantidade4) * Number(precos.picoleCoco) 
+    textoPagar4.textContent = total4
+  }
+  
+  function addProduto4 (){
+    const numero4 = parseInt(contador4.textContent) + 1 ;
+    contador4.textContent = numero4;  
+    picoleCoco()
+    
+  }
+  
+  function removeProduto4 (){
+  
+    if( contador4.textContent >= 1){
+      let numero4 = parseInt(contador4.textContent) - 1 ;
+      contador4.textContent = numero4
+      picoleCoco()
+     
+    } else{
+      contador4.textContent = 0
+      
+  
+    }
+  
+  }
 
+  //BOTÃO PICOLÉ-MANGA
+  const contador5 = document.querySelector('#qtd5');
+  const btnMais5 = document.querySelector('#btn5mais')
+  const btnMenos5 = document.querySelector('#btn5menos')
+  const textoPagar5 = document.querySelector('#total5') 
+  
+  btnMais5.addEventListener('click',addProduto5)
+  btnMenos5.addEventListener('click', removeProduto5)
+  
+  function picoleManga(){  
+    
+    let quantidade5 = contador5.textContent
+    const total5 = Number(quantidade5) * Number(precos.picoleManga) 
+    textoPagar5.textContent = total5
+  }
+  
+  function addProduto5 (){
+    const numero5 = parseInt(contador5.textContent) + 1 ;
+    contador5.textContent = numero5;  
+    picoleManga()
+    
+  }
+  
+  function removeProduto5 (){
+  
+    if( contador5.textContent >= 1){
+      let numero5 = parseInt(contador5.textContent) - 1 ;
+      contador5.textContent = numero5
+      picoleManga()
+     
+    } else{
+      contador5.textContent = 0
+      
+  
+    }
+  
+  }
+
+   //BOTÃO PICOLÉ-FRUTAS VERMELHAS
+   const contador6 = document.querySelector('#qtd6');
+   const btnMais6 = document.querySelector('#btn6mais')
+   const btnMenos6 = document.querySelector('#btn6menos')
+   const textoPagar6 = document.querySelector('#total6') 
+   
+   btnMais6.addEventListener('click',addProduto6)
+   btnMenos6.addEventListener('click', removeProduto6)
+   
+   function picoleFuVe(){  
+     
+     let quantidade6 = contador6.textContent
+     const total6 = Number(quantidade6) * Number(precos.picoleFuVe) 
+     textoPagar6.textContent = total6
+   }
+   
+   function addProduto6 (){
+     const numero6 = parseInt(contador6.textContent) + 1 ;
+     contador6.textContent = numero6;  
+     picoleFuVe()
+     
+   }
+   
+   function removeProduto6 (){
+   
+     if( contador6.textContent >= 1){
+       let numero6 = parseInt(contador6.textContent) - 1 ;
+       contador6.textContent = numero6
+       picoleFuVe()
+      
+     } else{
+       contador6.textContent = 0
+       
+   
+     }
+   
+   }
+
+    //BOTÃO PICOLÉ-Melancia
+    const contador7 = document.querySelector('#qtd7');
+    const btnMais7 = document.querySelector('#btn7mais')
+    const btnMenos7 = document.querySelector('#btn7menos')
+    const textoPagar7 = document.querySelector('#total7') 
+    
+    btnMais7.addEventListener('click',addProduto7)
+    btnMenos7.addEventListener('click', removeProduto7)
+    
+    function picoleMelancia(){  
+      
+      let quantidade7 = contador7.textContent
+      const total7 = Number(quantidade7) * Number(precos.picoleMelancia) 
+      textoPagar7.textContent = total7
+    }
+    
+    function addProduto7 (){
+      const numero7 = parseInt(contador7.textContent) + 1 ;
+      contador7.textContent = numero7;  
+      picoleMelancia()
+      
+    }
+    
+    function removeProduto7(){
+    
+      if(contador7.textContent >= 1){
+        let numero7 = parseInt(contador7.textContent) - 1 ;
+        contador7.textContent = numero7
+        picoleMelancia()
+       
+      } else{
+        contador7.textContent = 0
+      }
+    
+    }
+
+    //BOTÃO PICOLÉ-Abacaxi
+    const contador8 = document.querySelector('#qtd8');
+    const btnMais8 = document.querySelector('#btn8mais')
+    const btnMenos8 = document.querySelector('#btn8menos')
+    const textoPagar8 = document.querySelector('#total8') 
+    
+    btnMais8.addEventListener('click',addProduto8)
+    btnMenos8.addEventListener('click', removeProduto8)
+    
+    function picoleAbacaxi(){  
+      
+      let quantidade8 = contador8.textContent
+      const total8 = Number(quantidade8) * Number(precos.picoleAbacaxi) 
+      textoPagar8.textContent = total8
+    }
+    
+    function addProduto8(){
+      const numero8 = parseInt(contador8.textContent) + 1 ;
+      contador8.textContent = numero8;  
+      picoleAbacaxi()
+      
+    }
+    
+    function removeProduto8(){
+    
+      if(contador8.textContent >= 1){
+        let numero8 = parseInt(contador8.textContent) - 1 ;
+        contador8.textContent = numero8
+        picoleAbacaxi()
+       
+      } else{
+        contador8.textContent = 0
+      }
+    
+    }
   
